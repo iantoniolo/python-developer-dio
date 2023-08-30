39a315080fdd99361a81a7734156c91dde6a40e0
iantoniolo <iantoniolo@hotmail.com>
2023-08-30 12:34:33 -0300

    2° etapa - melhorando o sistema de banco

--- banking-system-challenge/main.py
+++ banking-system-challenge/main.py
@@ -1,64 +1,162 @@
-print(
+def menu():
+    print(
 """
 -------- SISTEMA BANCÁRIO --------
 
-    [1]: Visualizar saldo
-    [2]: Depositar
-    [3]: Sacar
-    [4]: Visualizar extrato
+    [1]: Cadastrar usuário
+    [2]: Criar conta
+    [3]: Visualizar saldo
+    [4]: Depositar
+    [5]: Sacar
+    [6]: Visualizar extrato
+    [7]: Listar usuários
+    [8]: Listar contas
     [0]: Sair
 
 ----------------------------------
 """
 )
 
-saldo = 0
-saque = 1
-extrato = []
+def visualizar_saldo(saldo):
+    print(f"Saldo disponível: R${saldo:.2f}")
 
-VALOR_LIMITE_SAQUE = 500
-QTD_LIMITE_SAQUE = 3
+def depositar(saldo, extrato, deposito):
+    valor_deposito = int(input('Qual valor (R$) deseja depositar em sua conta? Digite apenas números: '))
+    extrato += f"\nDepósito {deposito}: R${valor_deposito:.2f}"
+    saldo += valor_deposito
+    deposito += 1
+    print("Depósito concluído com sucesso.")
+    return saldo, extrato, deposito
 
-while True:
+def sacar(saque, saldo, extrato, QTD_LIMITE_SAQUE, VALOR_LIMITE_SAQUE):
+    if saque <= QTD_LIMITE_SAQUE:
+        valor_saque = int(input('Qual valor (R$) deseja sacar de sua conta? Digite apenas números: '))
+        if valor_saque <= saldo and valor_saque <= VALOR_LIMITE_SAQUE:
+            saldo -= valor_saque
+            extrato += f"\nSaque {saque}: R${valor_saque:.2f}"
+            saque += 1
+            print("Saque concluído com sucesso. Retire seu dinheiro na boca do caixa.")
+            return saque, saldo, extrato
+        elif valor_saque > saldo:
+            print("Saldo insuficiente. Não foi possível realizar a operação.")
+            return saque, saldo, extrato
+        elif valor_saque > VALOR_LIMITE_SAQUE:
+            print(f"Não foi possível realizar a operação. O valor limite para saque é {VALOR_LIMITE_SAQUE}")
+            return saque, saldo, extrato
+    else:
+        print("Limite diário de saques atingido. Tente novamente após 24 horas.")
+        return saque, saldo, extrato
+
+def visualizar_extrato(saldo, extrato):
+    print(f"""
+--------- EXTRATO ---------
+    {extrato}
+
+---------------------------
+Saldo da conta: R${saldo:.2f}
+---------------------------
+""")
     
-    opcao = int(input('\nDigite o número referente a operação em que deseja realizar: '))
+def criar_usuario(usuarios):
+    nome = input("Seu nome (completo): ")
+    cpf = input("CPF (Somente números): ")
+    data_nascimento = input("Data de nascimento (dd/mm/aaaa): ")
+    logradouro = input("Rua: ")
+    numero = input("Número: ")
+    bairro = input("Bairro: ")
+    cidade = input("Cidade: ")
+    sigla_estado = input("Sigla do estado: ")
+    endereco = f"{logradouro}, {numero} - {bairro} - {cidade}/{sigla_estado}"
 
-    if opcao == 1:
-        print(f"Saldo disponível: R${saldo},00")
+    usuario = {
+        "nome": nome,
+        "cpf": cpf,
+        "data_nascimento": data_nascimento,
+        "endereco": endereco
+    }
 
-    elif opcao == 2:
-        identificador_deposito = 1 
-        valor_deposito = int(input('Qual valor (R$) deseja depositar em sua conta? Digite apenas números: '))
-        saldo += valor_deposito
-        extrato.append(f"Depósito {identificador_deposito}: R${valor_deposito},00")
-        identificador_deposito += 1
-        print("Depósito concluído com sucesso.")
-
-    elif opcao == 3:
-        if saque <= QTD_LIMITE_SAQUE:
-            valor_saque = int(input('Qual valor (R$) deseja sacar de sua conta? Digite apenas números: '))
-            if valor_saque <= saldo:
-                saldo -= valor_saque
-                extrato.append(f"Saque {saque}: R${valor_saque},00")
-                saque += 1
-                print("Saque concluído com sucesso. Retire seu dinheiro na boca do caixa.")
-            elif valor_saque > saldo:
-                print("Saldo insuficiente. Não foi possível realizar a operação.")
+    if usuarios:
+        cpf_filtrado = []
+        for u in usuarios:
+            cpf_filtrado.append(u["cpf"])
+            
+        if usuario["cpf"] not in cpf_filtrado:
+            print("Checando CPF...")
+            usuarios.append(usuario)
+            print("CPF Disponível. Usuário cadastrado")
         else:
-            print("Limite diário de saques atingido. Tente novamente após 24 horas.")
-        
-    elif opcao == 4:
-        
-        print("------ EXTRATO ------")
-    
-        for e in extrato:
-            print(e)
+            print("Falha ao cadastrar. CPF indisponível!")
+    else:
+        print("Checando CPF...")
+        print("CPF Disponível. Usuário cadastrado")
+        usuarios.append(usuario)
 
-        print("---------------------")
-        
-    elif opcao == 0:
-        print("Obrigado por utilizar nosso sistema. Volte sempre!")
-        break
+def criar_conta(usuarios, contas, numero_conta, AGENCIA):
+    cpf = input("A qual CPF você deseja atrelar sua conta? ")
+
+    for usuario in usuarios:
+        if usuario["cpf"] == cpf:
+            usuario.setdefault("contas", [])
+            usuario["contas"].append({"agencia": AGENCIA, "numero_conta": numero_conta})
+            print("Conta criada com sucesso!")
+            contas.append({"agencia": AGENCIA, "numero_conta": numero_conta, "cpf_usuario": cpf})
+            numero_conta += 1
+            break
 
     else:
-        print("Opção inválida.")
+        print("Não existe um usuário atrelado a esse CPF.")
+
+    return numero_conta
+
+def main():
+
+    saldo = 0
+    saque = 1
+    deposito = 1
+    extrato = ""
+    usuarios = []
+    contas = []
+    numero_conta = 1
+
+    VALOR_LIMITE_SAQUE = 500
+    QTD_LIMITE_SAQUE = 3
+    AGENCIA = "0001"
+
+    menu()
+
+    while True:
+
+        opcao = int(input("\n=> Digite o número referente a operação em que deseja realizar: "))
+
+        if opcao == 1:
+            criar_usuario(usuarios=usuarios)
+
+        elif opcao == 2:
+            numero_conta = criar_conta(usuarios, contas, numero_conta, AGENCIA=AGENCIA)
+
+        elif opcao == 3:
+            visualizar_saldo(saldo=saldo)
+
+        elif opcao == 4:
+            saldo, extrato, deposito = depositar(saldo,extrato,deposito)
+
+        elif opcao == 5:
+            saque, saldo, extrato = sacar(saque=saque,saldo=saldo,extrato=extrato,QTD_LIMITE_SAQUE=QTD_LIMITE_SAQUE,VALOR_LIMITE_SAQUE=VALOR_LIMITE_SAQUE)
+            
+        elif opcao == 6:
+            visualizar_extrato(saldo, extrato=extrato)
+
+        elif opcao == 7:
+            print(usuarios) if usuarios else print("Sem registros de usuários.")
+
+        elif opcao == 8:
+            print(contas) if contas else print("Sem registros de contas.")
+            
+        elif opcao == 0:
+            print("Obrigado por utilizar nosso sistema. Volte sempre!")
+            break
+
+        else:
+            print("Opção inválida.")
+
+main()
